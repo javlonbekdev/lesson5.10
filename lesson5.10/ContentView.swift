cae35e30465c6a81019b4ef1d2358bb3d75ac2af
Javlonbek <javlonbekalhamdulillah@gmail.com>
2022-03-02 11:30:25 +0500

    push

--- lesson5.10/ContentView.swift
+++ lesson5.10/ContentView.swift
@@ -7,10 +7,57 @@
 
 import SwiftUI
 
+extension UIScreen{
+    static let width = UIScreen.main.bounds.size.width
+    static let height = UIScreen.main.bounds.size.height
+}
+
 struct ContentView: View {
+    @State private var str = ""
     var body: some View {
-        Text("Hello, world!")
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [Color.red, .yellow]), startPoint: .bottom, endPoint: .top)
+            
+            VStack{
+                HStack{
+                    Image(systemName: "person")
+                        .foregroundColor(.white)
+                    TextField("Enter your email", text: $str)
+                        .font(.system(size: 15))
+                }
+                .padding()
+                .overlay(RoundedRectangle(cornerRadius: 15).stroke(.white, lineWidth: 1))
+            
+                HStack{
+                    Image(systemName: "lock")
+                        .foregroundColor(.white)
+                    SecureField("Enter your password", text: $str)
+                        .font(.system(size: 15))
+                }
+                .padding()
+                .overlay(RoundedRectangle(cornerRadius: 15).stroke(.white, lineWidth: 1))
+                
+                HStack{
+                    Button {
+                    } label: {
+                        Text("Login")
+                            .foregroundColor(.white)
+                            .font(.system(size: 15))
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .overlay(RoundedRectangle(cornerRadius: 15).stroke(.white, lineWidth: 1))
+
+            }
             .padding()
+            
+        }
+        .ignoresSafeArea()
+        .safeAreaInset(edge: .bottom) {
+                Text("All rights reversed")
+                    .foregroundColor(.white)
+            }
     }
 }
 
